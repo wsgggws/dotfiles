5eba8557c46e135b7b6e6c3aaae6b2fd6bdfec14
Navy.Tian <hj.tian@aftership.com>
2024-09-24 17:55:58 +0800

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,15 +11,29 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
+config.color_scheme = "Solarized (dark) (terminal.sexy)"
+-- config.color_scheme = "Dracula (Official)"
 -- config.color_scheme = "Catppuccin Mocha"
-config.color_scheme = "tokyonight_night"
+-- config.color_scheme = "tokyonight_day"
 -- config.color_scheme = "Catppuccin Latte"
+-- config.color_scheme = "Gruvbox light, soft (base16)"
+-- config.colors = {
+-- 	background = "#EBDBB2",
+-- }
 
-config.font_size = 14.5
+config.font_size = 17
+config.font = wezterm.font("comic mono")
 config.window_decorations = "RESIZE"
+config.default_cursor_style = "BlinkingBar"
+-- config.default_cursor_style = "BlinkingUnderline"
 config.hide_tab_bar_if_only_one_tab = true
+config.scrollback_lines = 30000
+
+config.window_padding = {
+	top = 10,
+	bottom = 0,
+}
 
 config.keys = {
 	-- CTRL-SHIFT-l activates the debug overlay
