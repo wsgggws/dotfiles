4d9b4aca4cd5e0bdcd77b0b1993ea898b577a9f4
wsgggws <wsgggws@gmail.com>
2025-08-01 18:02:50 +0800

    update publish_web3 func

--- .zsh/functions/publish-web3.zsh
+++ .zsh/functions/publish-web3.zsh
@@ -2,6 +2,7 @@ publish_web3() {
   local prev_dir=$(pwd)
   cd "$HOME/peak" || return
   python gitlab_publish_k8s.py web3_property web3-property-lb &&
+  python gitlab_publish_k8s.py web3_property web3-property-celery &&
   python gitlab_publish_k8s.py web3_property web3-property &&
   python gitlab_publish_k8s.py web3_property web3-property-script
   cd "$prev_dir" || return
